4f7682bc0e8b7ce79966b3324a3a111dcb5631a6
alabo-excel <iamalaboexcel@gmail.com>
2023-12-07 15:30:01 +0100

    courses introduction commit

--- app/_components/AdminHeader.jsx
+++ app/_components/AdminHeader.jsx
@@ -7,13 +7,13 @@ const AdminHeader = () => {
         <div className='flex justify-between w-[40%]'>
           <div className='relative'>
             <input placeholder='Search' type="text" className='p-3 w-72 bg-white rounded-md text-sm pl-10' />
-            <img className='absolute w-5 h-5 top-3 left-2' src="../images/icons/search.svg" alt="" />
+            <img className='absolute w-5 h-5 top-3 left-2' src="/images/icons/search.svg" alt="" />
           </div>
           <div className='bg-white h-[45px] p-3 rounded-md'>
-            <img className=' w-5 h-5' src="../images/icons/message.svg" alt="" />
+            <img className=' w-5 h-5' src="/images/icons/message.svg" alt="" />
           </div>
           <div className='bg-white h-[45px] p-3 rounded-md'>
-            <img src="../images/icons/notifications.svg" alt="" />
+            <img src="/images/icons/notifications.svg" alt="" />
           </div>
         </div>
       </div>
