f39c4f1fd72c9097664d6d5eec32795c22762b41
alabo-excel <iamalaboexcel@gmail.com>
2024-06-24 19:43:26 +0100

    testing deployment

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,4 +1,4 @@
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
